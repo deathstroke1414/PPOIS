88ecee8ec4c319ea2dc08541a84d8db34aeb0c89
German Skovlyuk <113847176+deathstroke1414@users.noreply.github.com>
2024-02-29 16:54:41 +0300

    Add files via upload

--- lab2/test/test.cpp
+++ lab2/test/test.cpp
@@ -14,14 +14,27 @@
 #include <iterator>
 #include <string>
 
+
 using namespace std;
 
 TEST(HumanTest, DisplayInfo) {
     Human human("John Doe", 30);
     testing::internal::CaptureStdout();
     human.displayInfo();
+
+
     string output = testing::internal::GetCapturedStdout();
     EXPECT_EQ(output, "This is a human.\n");
+    string name = human.getName();
+    int age = human.getAge();
+    EXPECT_EQ(name, "John Doe");
+    EXPECT_EQ(age, 30);
+    human.setFIO("Johny Doe");
+    human.setAge(31);
+    name = human.getName();
+    age = human.getAge();
+    EXPECT_EQ(name, "Johny Doe");
+    EXPECT_EQ(age, 31);
 }
 
 TEST(BuilderTest, Tools) {
@@ -32,12 +45,16 @@ TEST(BuilderTest, Tools) {
 
 TEST(OfficeWorkerTest, SetOfficeNumber) {
     OfficeWorker officeWorker("German", 28, 3, 6000, 101);
+    officeWorker.displayInfo();
     officeWorker.setOfficeNumber(102);
     EXPECT_EQ(officeWorker.getOfficeNumber(), 102);
 }
 
 TEST(ApartmentTest, SetFloorAmount) {
     Apartment apartment;
+    apartment.displayInfo();
+    apartment.setBicycleParking(true);
+    apartment.setGarbageChute(false);
     apartment.setFloorAmount(5);
     EXPECT_EQ(apartment.getFloorAmount(), 5);
 }
@@ -45,5 +62,59 @@ TEST(ApartmentTest, SetFloorAmount) {
 TEST(CottageTest, SetBedroomsAmount) {
     Cottage cottage;
     cottage.setBedroomsAmount(3);
+    cottage.displayInfo();
     EXPECT_EQ(cottage.getBedroomsAmount(), 3);
+}
+
+TEST(ObjectTest, TestMethods) {
+    Object object;
+    Worker* worker = new Worker("Joe", 18, 99, 132.1);
+    worker->displayInfo();
+    worker->getSalary();
+    int expected = 99;
+    EXPECT_EQ(expected, worker->getExperience());
+    object.getId();
+    object.displayInfo();
+    object.addWorker(worker);
+
+    EXPECT_EQ(object.getWorkers().size(), 1);
+}
+
+
+TEST(PrivateHouseTest, ParametersTest) {
+    PrivateHouse prHouse;
+    prHouse.displayInfo();
+    prHouse.setRoomsAmount(3);
+    prHouse.setBathroomsAmount(1);
+
+}
+
+TEST(ResidentialBuildingTest, ParametersTest) {
+    ResidentialBuilding resBuilding;
+    resBuilding.displayInfo();
+    resBuilding.setGasPresence(false);
+    resBuilding.setHeater(true);
+    bool actual = resBuilding.getGasPresence();
+    bool expected = false;
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(PrivateHouseTest, DisplayInfo) {
+    PrivateHouse house;
+    // Ожидается, что метод просто выведет информацию о частном доме без ошибок
+    ASSERT_NO_THROW(house.displayInfo());
+}
+
+TEST(CottageTest, DefaultConstructor) {
+    Cottage cottage;
+
+    // Ожидаем, что все значения по умолчанию установлены правильно
+    ASSERT_EQ(cottage.getBedroomsAmount(), 0);
+}
+
+// Тестирование метода displayInfo
+TEST(CottageTest, DisplayInfo) {
+    Cottage cottage;
+    // Ожидаем, что метод просто выведет информацию о коттедже без ошибок
+    ASSERT_NO_THROW(cottage.displayInfo());
 }
